c169c9551f6fce8b6c53abee4e34331908365748
Louie Flores <louiejf1@gmail.com>
2020-11-04 22:09:36 -0600

    Sematic and cleaned up files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       My app is running
+      <button className="ui secondary button">Okay</button>
     </div>
   );
 }
